1c9dc80c1434d8a7f03a34a7d1d95d2122ddedc3
kshep425 <9432996+kshep425@users.noreply.github.com>
2020-01-30 00:11:03 -0500

    Reference did not work and threw an error, so I removed those and used associate to match it with Progress and Scores models

--- models/users.js
+++ models/users.js
@@ -1,13 +1,32 @@
+// const Progress = require("./progress")
+
 module.exports = function(sequelize, DataTypes) {
+    console.log("Create Users Table")
   var Users = sequelize.define("Users", {
     username: DataTypes.STRING,
-    score: DataTypes.BOOLEAN,
-    progress_id: {
-        references: {
-            model: Progress,
-            key: 'id'
-        }
-    }
+    score: DataTypes.BOOLEAN
+    // progress_id: {
+    //     type: DataTypes.INTEGER,
+    //     references: {
+    //         model: Progress,
+    //         key: 'id'
+    //     }
+    // }
   });
-  return Todo;
+
+  Users.associate = function(models) {
+      console.log("Did you reach here?")
+    models.Users.hasMany(models.Scores);
+    console.log("  - associate Score")
+}
+Users.associate = function(models){
+    models.Users.belongsTo(models.Progress, {
+        foreignKey: {
+            allowNull: false
+        }
+    })
+    console.log("  - associate Progress")
+  };
+
+  return Users;
 };
