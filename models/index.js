6425a4b766e95b41391f097d53d5a14149d1d420
kshep425 <9432996+kshep425@users.noreply.github.com>
2020-01-30 00:09:01 -0500

    Changed to es6; fixed user name, database, and host default names

--- models/index.js
+++ models/index.js
@@ -1,17 +1,19 @@
 "use strict";
 
+require("dotenv").config()
+
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
-var basename = path.basename(module.filename);
-
+var basename = path.basename(__filename);
+const initialize_db = require("../config/db_connection")
 var db = {};
 
 const db_obj = {
-    database: process.env.DB_NAME || "jkready2",
-    username: process.env.DB_USER || "root2",
+    database: process.env.DB_NAME || "jkready",
+    username: process.env.DB_USER || "root",
     password: process.env.DB_PW,
-    host: process.env.DB_HOST || "localhost2"
+    host: process.env.DB_HOST || "localhost"
 }
 console.log(db_obj)
 
@@ -19,15 +21,16 @@ const sequelize = initialize_db(db_obj)
 
 fs
   .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
-  .forEach(function(file) {
-    var model = sequelize["import"](path.join(__dirname, file));
+  .forEach(file => {
+    const model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(function(modelName) {
+Object.keys(db).forEach(modelName => {
+    console.log("model Name: " + modelName)
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
