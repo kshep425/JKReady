43d3e872af1fc3e4a0558296c73baadc2c463904
kshep425 <9432996+kshep425@users.noreply.github.com>
2020-01-30 11:37:59 -0500

    Removed commented code, formatted document

--- models/progress.js
+++ models/progress.js
@@ -1,40 +1,33 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     console.log('Create Progress table')
-  var Progress = sequelize.define("Progress", {
-    level: DataTypes.STRING,
-    question: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    correct_answer: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    wrong_answer_1: DataTypes.STRING,
-    wrong_answer_2: DataTypes.STRING,
-    points: {
-        type: DataTypes.INTEGER,
-        defaultValue: 5,
-        allowNull: false
-    },
-    prev_question: {
-        type: DataTypes.INTEGER,
-        // references: {
-        //     model: Progress,
-        //     key: 'id'
-        // }
-    },
-    next_question: {
-        type: DataTypes.INTEGER,
-        // references: {
-        //     model: Progress,
-        //     key: 'id'
-        // }
-     }
-});
+    var Progress = sequelize.define("Progress", {
+        level: DataTypes.STRING,
+        question: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        correct_answer: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        wrong_answer_1: DataTypes.STRING,
+        wrong_answer_2: DataTypes.STRING,
+        points: {
+            type: DataTypes.INTEGER,
+            defaultValue: 5,
+            allowNull: false
+        },
+        prev_question: {
+            type: DataTypes.INTEGER,
+        },
+        next_question: {
+            type: DataTypes.INTEGER,
+        }
+    });
 
-  Progress.associate = function(models) {
-    models.Progress.hasMany(models.Users);
-  }
-  return Progress;
+    Progress.associate = function (models) {
+        models.Progress.hasMany(models.Users);
+    }
+
+    return Progress;
 };
