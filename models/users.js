a977e0a5c381d0a89813b27cf0d70305ea74446e
kshep425 <9432996+kshep425@users.noreply.github.com>
2020-01-30 11:38:42 -0500

    Added password that is saved as a hash

--- models/users.js
+++ models/users.js
@@ -1,32 +1,47 @@
-// const Progress = require("./progress")
+var bcrypt = require("bcryptjs");
 
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     console.log("Create Users Table")
-  var Users = sequelize.define("Users", {
-    username: DataTypes.STRING,
-    score: DataTypes.BOOLEAN
-    // progress_id: {
-    //     type: DataTypes.INTEGER,
-    //     references: {
-    //         model: Progress,
-    //         key: 'id'
-    //     }
-    // }
-  });
+    var Users = sequelize.define("Users", {
+        // The username to login to the site
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
 
-  Users.associate = function(models) {
-      console.log("Did you reach here?")
-    models.Users.hasMany(models.Scores);
-    console.log("  - associate Score")
-}
-Users.associate = function(models){
-    models.Users.belongsTo(models.Progress, {
-        foreignKey: {
+        // The password cannot be null
+        password: {
+            type: DataTypes.STRING,
             allowNull: false
         }
-    })
-    console.log("  - associate Progress")
-  };
+    });
 
-  return Users;
+    // This allows user id to be a foreign key in the Scores table
+    Users.associate = function (models) {
+        models.Users.hasMany(models.Scores);
+    }
+
+    // This allows progress id to be a foreign key in the Users table.
+    Users.associate = function (models) {
+        models.Users.belongsTo(models.Progress, {
+            foreignKey: {
+                allowNull: false
+            },
+            defaultValue: 0
+        })
+        console.log("  - associate Progress")
+    };
+
+    // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+    Users.prototype.validPassword = function (password) {
+        return bcrypt.compareSync(password, this.password);
+    };
+
+    // Before a User is created, we will automatically hash their password
+    Users.addHook("beforeCreate", function (user) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    });
+
+    return Users;
 };
