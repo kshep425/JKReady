6624b76124e198dd512bc5cb3c9021210857e2a5
kshep425 <9432996+kshep425@users.noreply.github.com>
2020-01-30 20:07:50 -0500

    Added express app and login_api_routes

--- server.js
+++ server.js
@@ -5,25 +5,38 @@
 // *** Dependencies
 // =============================================================
 var express = require("express");
-
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
 // Sets up the Express App
 // =============================================================
-var app = express();
+
+// Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
 var db = require("./models");
 
+
+// Creating express app and configuring middleware needed for authentication
+var app = express();
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 // Static directory
 app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // Routes
 // =============================================================
-//require("./routes/api_routes")(app);
+//const api_routes = require("./routes/api_routes")
+//app.use(api_routes);
+require("./routes/login_api_routes")(app)
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
