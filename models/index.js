bd9393a74f6a226440a5cfeea719142a3fd7cc67
kshep425 <9432996+kshep425@users.noreply.github.com>
2020-01-30 20:06:06 -0500

    Removed the hard coded db obj, it now gets it from the config file

--- models/index.js
+++ models/index.js
@@ -1,20 +1,13 @@
 "use strict";
 
-require("dotenv").config()
-
-var fs = require("fs");
-var path = require("path");
-var Sequelize = require("sequelize");
-var basename = path.basename(__filename);
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const basename = path.basename(__filename);
 const initialize_db = require("../config/db_connection")
-var db = {};
+let db = {};
 
-const db_obj = {
-    database: process.env.DB_NAME || "jkready",
-    username: process.env.DB_USER || "root",
-    password: process.env.DB_PW,
-    host: process.env.DB_HOST || "localhost"
-}
+const db_obj = require("../config/db_config")
 
 const sequelize = initialize_db(db_obj)
 
