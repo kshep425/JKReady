152c077aeacfca744a61cc9543e9251b78b126ab
Robin Jones <RobinJones335@gmail.com>
2020-02-01 15:06:22 -0500

    added correct background

--- phaser/src/index.js
+++ phaser/src/index.js
@@ -1,44 +1,22 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS,{preload: preload, create: create});
-// import Phaser from "phaser";
-// import logoImg from "./assets/test.JPG";
-import bkImg from "./assets/test.JPG";
-
-var config = {
-  type: Phaser.CANVAS,
-  width: 800,
-  height: 600,
-  physics: {
-      default: 'arcade',
-      arcade: {
-          gravity: { y: 200 }
-      }
-  },
-  scene: {
-      preload: preload,
-      create: create
-  }
-};
-
-const game = new Phaser.Game(config);
+import Phaser from 'phaser';
 
 function preload() {
-  game.load.image('bkImg',);
-  // this.load.image("logo", logoImg);
-}
-
-function create(){
-   game.add.image(game.world.centerX, game.world.centerY, 'hotdog').anchor.set(0.5);
+  this.load.image('sky', 'src/assets/canvas.jpg')
 }
 
 function create() {
-  const logo = this.add.image(800, 600, "logo");
-
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+  this.add.image(250, 230, 'sky')
 }
+
+const config = {
+	type: Phaser.AUTO,
+	width: 900,
+	height: 600,
+	backgroundColor: "#007eff",
+	scene: {
+    create,
+    preload
+	}
+}
+
+const game = new Phaser.Game(config)
