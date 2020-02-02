1c385e7a07ad30eca37cb4ace1014a13447edde9
Robin Jones <RobinJones335@gmail.com>
2020-02-02 09:03:18 -0500

    adding all charaters

--- phaser/src/index.js
+++ phaser/src/index.js
@@ -1,11 +1,19 @@
 import Phaser from 'phaser';
+let bad;
+let good;
 
 function preload() {
   this.load.image('sky', 'src/assets/canvas.jpg')
+  this.load.image('bad','src/assets/reaper.png')
+  this.load.image('good', 'src/assets/megaman moving-1.png')
 }
 
 function create() {
   this.add.image(250, 230, 'sky')
+  bad = this.add.sprite(200, 200, 'bad')
+} good = this.add.sprite(180, 230, 'good')
+function update() {
+	bad.x += 1;
 }
 
 const config = {
@@ -14,8 +22,9 @@ const config = {
 	height: 600,
 	backgroundColor: "#007eff",
 	scene: {
-    create,
-    preload
+	preload,
+	create,
+	update
 	}
 }
 
