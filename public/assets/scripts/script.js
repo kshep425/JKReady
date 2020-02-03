84aa94492b33062cc1871be2b8d0e29515567d74
kshep425 <9432996+kshep425@users.noreply.github.com>
2020-02-03 18:43:49 -0500

    Added document.ready around file text

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,57 +1,61 @@
-let animationInterval;
-// let widthOfSpriteSheet = 168;
-// let widthOfEachSprite = 56;
+$(document).ready(function () {
 
-function stop_animation() {
-  clearInterval(animationInterval);
-}
+    let animationInterval;
+    // let widthOfSpriteSheet = 168;
+    // let widthOfEachSprite = 56;
 
-function villain_start_animation() {
-  let widthOfSpriteSheet = 168;
-  let widthOfEachSprite = 56;
-
-  let position = widthOfEachSprite; //start position for the image
-  let speed = 150; //in millisecond(ms)
-  let diff = widthOfEachSprite; //difference between two sprites
-
-  animationInterval = setInterval(() => {
-    var spriteSheet = document.getElementById("villain-sprite-image");
-
-    spriteSheet.style.backgroundPosition = `-${position}px 0px`;
-
-    if (position < widthOfSpriteSheet) {
-      position = position + diff;
-    } else {
-      //increment the position by the width of each sprite each time
-      position = widthOfEachSprite;
+    function stop_animation() {
+        clearInterval(animationInterval);
     }
-    //reset the position to show first sprite after the last one
-  }, speed);
-}
 
-function hero_start_animation() {
-  let widthOfSpriteSheet = 448;
-  let widthOfEachSprite = 56;
+    function villain_start_animation() {
+        let widthOfSpriteSheet = 168;
+        let widthOfEachSprite = 56;
 
-  let position = widthOfEachSprite; //start position for the image
-  let speed = 200; //in millisecond(ms)
-  let diff = widthOfEachSprite; //difference between two sprites
+        let position = widthOfEachSprite; //start position for the image
+        let speed = 150; //in millisecond(ms)
+        let diff = widthOfEachSprite; //difference between two sprites
 
-  animationInterval = setInterval(() => {
-    var spriteSheet = document.getElementById("hero-sprite-image");
-    spriteSheet.style.backgroundPosition = `-${position}px 0px`;
+        animationInterval = setInterval(() => {
+            var spriteSheet = document.getElementById("villain-sprite-image");
 
-    if (position < widthOfSpriteSheet) {
-      position = position + diff;
-    } else {
-      //increment the position by the width of each sprite each time
-      position = widthOfEachSprite;
+            spriteSheet.style.backgroundPosition = `-${position}px 0px`;
+
+            if (position < widthOfSpriteSheet) {
+                position = position + diff;
+            } else {
+                //increment the position by the width of each sprite each time
+                position = widthOfEachSprite;
+            }
+            //reset the position to show first sprite after the last one
+        }, speed);
     }
-    //reset the position to show first sprite after the last one
-  }, speed);
-}
+
+    function hero_start_animation() {
+        let widthOfSpriteSheet = 448;
+        let widthOfEachSprite = 56;
+
+        let position = widthOfEachSprite; //start position for the image
+        let speed = 200; //in millisecond(ms)
+        let diff = widthOfEachSprite; //difference between two sprites
+
+        animationInterval = setInterval(() => {
+            var spriteSheet = document.getElementById("hero-sprite-image");
+            spriteSheet.style.backgroundPosition = `-${position}px 0px`;
+
+            if (position < widthOfSpriteSheet) {
+                position = position + diff;
+            } else {
+                //increment the position by the width of each sprite each time
+                position = widthOfEachSprite;
+            }
+            //reset the position to show first sprite after the last one
+        }, speed);
+    }
 
 
-//Start animation
-villain_start_animation();
-hero_start_animation();
+    //Start animation
+    villain_start_animation();
+    hero_start_animation();
+
+});
