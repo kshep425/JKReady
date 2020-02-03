21dc6c4a439ef25db7b84bbbde9a67238e244d57
kshep425 <9432996+kshep425@users.noreply.github.com>
2020-02-03 18:44:47 -0500

    Added login page and index page

--- routes/index_routes.js
+++ routes/index_routes.js
@@ -21,12 +21,11 @@ module.exports = function (app) {
     app.get("/", function (req,res){
         console.log("Open Homepage");
         if (req.user) {
-            console.log("You are logged in as:")
-            console.log(req.user)
-            res.sendFile(path.join(__dirname + "../public/assets/index.html"))
+            console.log("You are logged in as: " + req.user.username)
+            res.sendFile(path.join(__dirname, "/../public/assets/index.html"))
         } else {
             console.log("You need to login")
-            res.sendFile(path.join(__dirname, "/../public/assets/index.html"))
+            res.sendFile(path.join(__dirname, "/../public/assets/login.html"))
         }
 
     })
