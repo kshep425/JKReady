6aaf497a734b77ed24bfb763a90a3802ae181507
kshep425 <9432996+kshep425@users.noreply.github.com>
2020-02-04 01:55:44 -0500

    Added hard coded score

--- routes/index_routes.js
+++ routes/index_routes.js
@@ -20,7 +20,7 @@ module.exports = function (app) {
         console.log("Open High Scores Page")
         if (req.user) {
             console.log("You are logged in as: " + req.user.username)
-            let high_scores = {scores: {username: "test_username", score: "0"}}
+            let high_scores = {scores: [{username: "test_username", score: "0"}]}
             console.log(high_scores)
             res.render("index", high_scores)
         } else {
