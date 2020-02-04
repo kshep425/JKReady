7a23fcbd78e01f6977d09a739b9f5407c8f02bed
kshep425 <9432996+kshep425@users.noreply.github.com>
2020-02-04 15:56:27 -0500

    Added various routes...they all don't route somewhere yet

--- routes/index_routes.js
+++ routes/index_routes.js
@@ -4,23 +4,23 @@ var db = require("../models");
 
 module.exports = function (app) {
 
-    app.get("/", function (req,res){
+    app.get("/", function (req, res) {
         console.log("Open Homepage");
         if (req.user) {
             console.log("You are logged in as: " + req.user.username)
-            res.sendFile(path.join(__dirname, "/../public/assets/index.html"))
+            res.render("index", {username: username})
         } else {
             console.log("You need to login")
-            res.sendFile(path.join(__dirname, "/../public/assets/login.html"))
+            res.render("index")
         }
 
     })
 
-    app.get("/scores", function (req,res){
+    app.get("/scores", function (req, res) {
         console.log("Open High Scores Page")
         if (req.user) {
             console.log("You are logged in as: " + req.user.username)
-            let high_scores = {scores: [{username: "test_username", score: "0"}]}
+            let high_scores = { scores: [{ username: "test_username", score: "0" }] }
             console.log(high_scores)
             res.render("index", high_scores)
         } else {
@@ -28,6 +28,48 @@ module.exports = function (app) {
             res.sendFile(path.join(__dirname, "/../public/assets/login.html"))
         }
 
+    })
+
+    app.get("/login", function (req, res) {
+        console.log("Open Login Form")
+        if (req.user) {
+            console.log("You are logged in as: " + req.user.username)
+            res.render("index")
+        } else {
+            console.log("You need to login")
+            res.render("index")
+        }
+    })
+
+    app.get("/logout", function (req, res) {
+        console.log("User has logged out")
+    })
+
+    app.get("/intro", function (req, res) {
+        console.log("Start Game Introduction")
+        if(req.user){
+            res.render("index", {username: req.body.username})
+        } else {
+            console.log("You need to login")
+            res.render("index")
+        }
+
+    })
+
+    app.get("/game", function (req, res) {
+        console.log("Start Game and display game board")
+    })
+
+    app.get("/contact", function (req, res) {
+        console.log("Display contact us form")
+    })
+
+    app.get("/instructions", function (req, res) {
+        console.log("Display Instructions")
+    })
+
+    app.get("/gameover", function (req, res) {
+        console.log("Game Over")
     })
 
 }
