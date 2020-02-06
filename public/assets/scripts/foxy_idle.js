266796672bfaa1d092bddfd5c90d8b84039e1ba4
JGen1 <jengenes01@gmail.com>
2020-02-06 02:27:32 -0500

    Layout changes for game page.

--- public/assets/scripts/foxy_idle.js
+++ public/assets/scripts/foxy_idle.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var animationInterval;
+    let animationInterval;
     let widthOfSpriteSheet = 448;
     let widthOfEachSprite = 56;
     let spriteSheet = document.getElementById("foxy-fixer-idle-speak");
@@ -8,9 +8,10 @@ $(document).ready(function () {
     hero_start_animation();
 
     function hero_start_animation() {
-        let position = widthOfEachSprite; //start position for the image
+        let position = 56; //start position for the image
         let speed = 200; //in millisecond(ms)
-        let diff = widthOfEachSprite; //difference between two sprites
+        let diff = widthOfEachSprite; 
+        //difference between two sprites
         animationInterval = setInterval(() => {
             spriteSheet.style.backgroundPosition = `-${position}px 0px`;
 
