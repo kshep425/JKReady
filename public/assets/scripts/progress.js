b65c9c13c31ba66d8665135aae6f0b1e7349ae9d
JGen1 <jengenes01@gmail.com>
2020-02-07 01:05:36 -0500

    Latest fix for displaying question by id,added link to jquery ui, modified progress.js to include button attributes. Still need to add 'image' to progress table to retrieve image src.

--- public/assets/scripts/progress.js
+++ public/assets/scripts/progress.js
@@ -1,32 +1,86 @@
 $(document).ready(function () {
-
-    let $quiz_container = $(".quiz-container");
     let intro_form = $("#intro_form");
-    let questions = [];
-    //get_questions();
-    // This function resets the questions displayed with new questions from the database
-    function initialize_rows(questions) {
-        $quiz_container.empty();
-        var rowsToAdd = [];
-        for (var i = 0; i < questions.length; i++) {
-            rowsToAdd.push(questions[i]);
-        }
-        $quiz_container.prepend(rowsToAdd);
-        console.log(rowsToAdd);
-    }
-    // This function grabs questinos from the database and updates the questions view
-    function get_questions() {
-        $.get("/api/questions/3", function (data) {
-            questions = data;
-            initialize_rows(questions);
-        });
-    }
+
+    var image = $("#world");
+
+
+    // function fixed_image() {
+    //     console.log(image.src);
+    //     switch (image.src) {
+    //         case "fixed_image_1":
+    //             image.src = "broken_image_2.png"
+    //             break;
+    //         case "fixed_image_2":
+    //             image.src = "fixed_image_3.png"
+    //             break;
+    //         default:
+    //             console.log('No image found');
+    //     }
+    //     console.log(image.src);
+    // }
+
+
+//     if (image.src.match("broken_image_1")) {
+//         image.src = "fixed_image_1";
+//     }
+//     else if (image.src.match("broken_image_2")) {
+//         image.src = "fixed_image_2";
+//     } else
+//         if (image.src.match)
+// }
+// }
+
+
+
+
+    // function pause_next() {
+    //     setTimeout(function () { alert_modal(); }, 3000);
+    // }
+
 
     intro_form.on("submit", function (event) {
         event.preventDefault();
         console.log("Start Game by getting questions")
-        get_questions();
+
         window.location.replace("/game")
     })
+
+    $("#correct_answer").click(function (event) {
+        event.preventDefault();
+        console.log("correct answer clicked");
+        // cheer_modal();
+        // fixed_image();
+        let next_id = $("#next_question_id").attr("data-next_question_id")
+        console.log(next_id);
+        $.ajax({
+            url: "/api/user_data",
+            type: 'PUT',
+            data: { ProgressId: next_id || 3 },
+            success: function () {
+                window.location.replace("/game");
+            }
+        })
+    })
+   $(".wrong").click(function (event) {
+        event.preventDefault();
+        console.log($(this))
+        $( ".container" ).effect( "shake", {}, 500, function(){
+            //add sound
+            console.log("WRONG ANSWER!!!!")
+        } );
+
+        if ($(this).attr().includes("wrong")){
+            $( "#world" ).effect( "shake", {}, 500, function(){
+                //add sound
+                console.log("WRONG ANSWER!!!!")
+            } );
+        } else {
+            $("#world").effect("Fade", {}, 1000, function(){
+                console.log("You're Right")
+            })
+        }
+    
+  })
 });
+
 
