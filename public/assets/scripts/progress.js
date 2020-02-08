fa160ce4cf04c49fc60588b15b06759a6215ba6e
JGen1 <jengenes01@gmail.com>
2020-02-08 05:00:19 -0500

    ...

--- public/assets/scripts/progress.js
+++ public/assets/scripts/progress.js
@@ -2,53 +2,28 @@ $(document).ready(function () {
     let intro_form = $("#intro_form");
 
     var image = $("#world");
-
-
-    // function fixed_image() {
-    //     console.log(image.src);
-    //     switch (image.src) {
-    //         case "fixed_image_1":
-    //             image.src = "broken_image_2.png"
-    //             break;
-    //         case "fixed_image_2":
-    //             image.src = "fixed_image_3.png"
-    //             break;
-    //         default:
-    //             console.log('No image found');
-    //     }
-    //     console.log(image.src);
-    // }
-
-
-//     if (image.src.match("broken_image_1")) {
-//         image.src = "fixed_image_1";
-//     }
-//     else if (image.src.match("broken_image_2")) {
-//         image.src = "fixed_image_2";
-//     } else
-//         if (image.src.match)
-// }
-// }
-
-
-
-
-    // function pause_next() {
-    //     setTimeout(function () { alert_modal(); }, 3000);
-    // }
-
-
+    function playAudio(assistant) {
+        let text_to_speech = '<speak>'
+            + 'I can play a sound'
+            + '<audio src="https://actions.google.com/sounds/v1/alarms/digital_watch_alarm_long.ogg">a digital watch alarm</audio>. '
+            + '</speak>'
+        assistant.tell(text_to_speech);
+    };
     intro_form.on("submit", function (event) {
         event.preventDefault();
         console.log("Start Game by getting questions")
-
         window.location.replace("/game")
+        playAudio();
     })
+    function play_audio() {
+        var audio = document.getElementById("audio");
+        audio.play();
+    }
 
     $("#correct_answer").click(function (event) {
         event.preventDefault();
         console.log("correct answer clicked");
-        // cheer_modal();
+        play_audio();
         // fixed_image();
         let next_id = $("#next_question_id").attr("data-next_question_id")
         console.log(next_id);
@@ -60,27 +35,29 @@ $(document).ready(function () {
                 window.location.replace("/game");
             }
         })
+        sound.play();
     })
-   $(".wrong").click(function (event) {
+    $(".wrong").click(function (event) {
         event.preventDefault();
         console.log($(this))
-        $( ".container" ).effect( "shake", {}, 500, function(){
+        $(".container").effect("shake", {}, 500, function () {
             //add sound
             console.log("WRONG ANSWER!!!!")
-        } );
+            // sound.play();
+        });
 
-        if ($(this).attr().includes("wrong")){
-            $( "#world" ).effect( "shake", {}, 500, function(){
+        if ($(this).attr().includes("wrong")) {
+            $("#world").effect("shake", {}, 500, function () {
                 //add sound
                 console.log("WRONG ANSWER!!!!")
-            } );
+            });
         } else {
-            $("#world").effect("Fade", {}, 1000, function(){
+            $("#world").effect("Fade", {}, 1000, function () {
                 console.log("You're Right")
             })
         }
-    
-  })
+
+    })
 });
 
 
