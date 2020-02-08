31c97ab74126cc89979349e633492f20993850d5
JGen1 <jengenes01@gmail.com>
2020-02-08 02:27:57 -0500

    ...

--- config/db_config.js
+++ config/db_config.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const db_obj = {
     database: process.env.DB_NAME || "jkready_db",
     username: process.env.DB_USER || "root",
-    password: process.env.DB_PW || "Biodesi647025!",
+    password: process.env.DB_PW,
     host: process.env.DB_HOST || "localhost"
 }
 
