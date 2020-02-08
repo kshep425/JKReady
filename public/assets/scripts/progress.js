31ef84ea0c4df4cafd725fabda096d84b5eadb91
Keisha Shepherd <kshep425@gmail.com>
2020-02-08 03:32:28 -0500

    Added game_over and update world capability

--- public/assets/scripts/progress.js
+++ public/assets/scripts/progress.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
     let intro_form = $("#intro_form");
 
-    let image = $("#world");
-
     intro_form.on("submit", function (event) {
         event.preventDefault();
         console.log("Start Game by getting questions")
@@ -18,21 +16,31 @@ $(document).ready(function () {
         let response = $("#correct_answer").attr("data-response")
         display_response(response);
         console.log(next_id);
-        $.ajax({
-            url: "/api/user_data",
-            type: 'PUT',
-            data: { ProgressId: next_id || 1 },
-            success: function () {
+        update_world()
+            .then(function () {
+
                 $.ajax({
-                    url: "/api/user_score",
+                    url: "/api/user_data",
                     type: 'PUT',
-                    data: {
-                        score: 5
+                    data: { ProgressId: next_id || 1 },
+                    success: function () {
+                        $.ajax({
+                            url: "/api/user_score",
+                            type: 'PUT',
+                            data: {
+                                score: 5
+                            }
+                        })
+                        console.log("next_id: " + next_id)
+                        if (next_id) {
+                            window.location.replace("/game/" + next_id);
+                        } else {
+                            console.log("Game Over")
+                            game_over();
+                        }
                     }
                 })
-                window.location.replace("/game/" + next_id);
-            }
-        })
+            })
     })
 
     $(".wrong").click(function (event) {
@@ -58,7 +66,38 @@ $(document).ready(function () {
         })
     })
 
-    function display_response(response){
+    function display_response(response) {
         $("#answer_response").text(response)
+    }
+
+    function update_world() {
+        return new Promise(function (resolve, reject) {
+            let correct_src = "/assets/images/" + $("#world").data("correct")
+            console.log("correct_src: " + correct_src)
+            $("#world").attr("src", correct_src).effect("puff", {}, 5000)
+            setTimeout(function () {
+                console.log("Finished puffing")
+                $(".blackboard").hide()
+                resolve()
+            }, 4000)
+        })
+    }
+
+    function game_over() {
+        console.log("Game Over")
+        $(".question").hide();
+        $(".answers").hide();
+        $(".blackboard").show();
+        $("#answer_response").effect("bounce", {}, 20000).text("GAME OVER").show();
+        $.ajax({
+            url: "/api/user_score",
+            type: 'GET'
+        })
+            .then((response) => {
+                console.log(response)
+                $("p").effect("bounce", {}, 20000).html("<div>You Scored " + response.score + " points<br>Let's see how you did compared to everyone else<div>")
+
+                $("#high_scores_div").show().effect("bounce", {}, 20000);
+            })
     }
 });
